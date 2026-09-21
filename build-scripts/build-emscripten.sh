75bebb4a5dc1c301c07c513bca53270c1597a02a
wolferd-wolf <akilawolfs@gmail.com>
2026-09-21 09:39:16 +0530

    Enable Emscripten LZ4 support for web data package

--- build-scripts/build-emscripten.sh
+++ build-scripts/build-emscripten.sh
@@ -27,5 +27,8 @@ pkg-config --exists freetype2
 
 CXX_PKG_CFLAGS="$(pkg-config --cflags sdl3 sdl3-image sdl3-ttf freetype2 zlib)"
 CXX_PKG_LIBS="$(pkg-config --libs sdl3 sdl3-image sdl3-ttf freetype2 zlib)"
+# The web data package is generated with file_packager --lz4, so the main
+# Emscripten runtime must include its LZ4 decompressor support.
+CXX_PKG_LZ4="-sLZ4=1"
 
-make -j$(nproc)   NATIVE=emscripten   BACKTRACE=0   TILES=1   SOUND=0   TESTS=0   RUNTESTS=0   RELEASE=1   CCACHE="$CCACHE"   LINTJSON=0   PKG_CONFIG=pkg-config   CXXFLAGS="$CXX_PKG_CFLAGS"   LDFLAGS="$CXX_PKG_LIBS"   cataclysm-tiles.js
+make -j$(nproc)   NATIVE=emscripten   BACKTRACE=0   TILES=1   SOUND=0   TESTS=0   RUNTESTS=0   RELEASE=1   CCACHE="$CCACHE"   LINTJSON=0   PKG_CONFIG=pkg-config   CXXFLAGS="$CXX_PKG_CFLAGS"   LDFLAGS="$CXX_PKG_LIBS" $CXX_PKG_LZ4   cataclysm-tiles.js
